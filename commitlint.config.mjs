17c99d9d5cadfa3119f88d81d488762da3865ae0
MAXLZ <2251683416@qq.com>
2024-12-08 16:49:06 +0800

    chore: allow release type git message

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,22 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'build',
+        'ci',
+        'perf',
+        'release',
+      ],
+    ],
+  },
+};
