576682968bea9155d8c201b9bb95e49f635c70e9
MAXLZ <2251683416@qq.com>
2024-12-09 20:30:34 +0800

    chore: add lint and format script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ['dist/*', 'coverage/*'] },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
