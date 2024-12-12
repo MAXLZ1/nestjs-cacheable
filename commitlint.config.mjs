55677eff1c819ff788472772f385b1b04074fc8b
MAXLZ <2251683416@qq.com>
2024-12-12 22:14:53 +0800

    chore: update commitlint rule

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,23 +1,3 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'release',
-      ],
-    ],
-  },
 };
